affeee5db6eb512184997d475dc39f23278f8906
Jason Wang <jason@wangjason.com>
2018-09-05 16:36:41 -0700

    removed garbage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import FormContainer from "./js/components/container/FormContainer";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class FormContainer extends Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+const wrapper = document.getElementById("app");
+ReactDOM.render(<FormContainer />, wrapper);
