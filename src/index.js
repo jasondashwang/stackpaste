b19baf03a257ab88f15f9829536e4fc48b7bc0f5
Jason Wang <jason@wangjason.com>
2018-09-06 17:25:42 -0700

    basic stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class FormContainer extends Component {
-  render() {
-    return (
-      <div>Hello World</div>
-    );
-  }
-}
-
-const wrapper = document.getElementById("app");
-ReactDOM.render(<FormContainer />, wrapper);
+const wrapper = document.getElementById('app');
+ReactDOM.render(null, wrapper);
